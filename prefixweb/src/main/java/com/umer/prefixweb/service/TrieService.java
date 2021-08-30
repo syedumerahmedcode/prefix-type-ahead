dbdf88a61d5d68a614c3c3b0c9cb6291d8186bde
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-30 17:12:49 +0200

    Feature: Added methods for TrieService which insert and retrive Trie from the prefix backend logic.

--- prefixweb/src/main/java/com/umer/prefixweb/service/TrieService.java
+++ prefixweb/src/main/java/com/umer/prefixweb/service/TrieService.java
@@ -1,5 +1,7 @@
 package com.umer.prefixweb.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import data.PrefixTrie;
@@ -20,6 +22,12 @@ public class TrieService {
 		this.prefixTrie=new PrefixTrie();
 	}
 	
+	public List<String> getMatchingPhrases(String prefix){
+		return prefixTrie.findMatchingPhrases(prefix);
+	}
 	
+	public void insertPrefix(String prefix) {
+		prefixTrie.insertPrefix(prefix);
+	}
 	
 }
