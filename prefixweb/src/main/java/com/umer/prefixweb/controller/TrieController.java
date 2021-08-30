14fae29f1e14ac8086e0abe541da082fa9ea8cb1
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-30 17:13:44 +0200

    Feature: Added a get controller method for health check.

--- prefixweb/src/main/java/com/umer/prefixweb/controller/TrieController.java
+++ prefixweb/src/main/java/com/umer/prefixweb/controller/TrieController.java
@@ -1,17 +1,29 @@
 package com.umer.prefixweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umer.prefixweb.service.TrieService;
+
 @RestController
 @RequestMapping("/api/v1/trie")
 public class TrieController {
 	
+	@Autowired
+	private TrieService trieService;
+	
 	@GetMapping("/insert/{prefix}")
 	public String addPrefix(@PathVariable("prefix") String prefix) {
-		throw new UnsupportedOperationException("Method not implemented");
+		trieService.insertPrefix(prefix);
+		return prefix + " was inserted into the trie successfully";
+	}
+	
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "I am alive";
 	}
 
 }
