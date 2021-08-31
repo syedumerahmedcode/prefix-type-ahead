2cea1ef9c2d222053707c062cfea0cc21e9bae28
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-31 02:51:58 +0200

    Feature: Added a RedisTemplate method in the configuration.

--- prefixweb/src/main/java/com/umer/prefixweb/configuration/RedisConfiguration.java
+++ prefixweb/src/main/java/com/umer/prefixweb/configuration/RedisConfiguration.java
@@ -4,15 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
-	
+
 	@Bean
 	JedisConnectionFactory getJedisConnectionFactory() {
-		RedisStandaloneConfiguration redisStandaloneConfiguration=new RedisStandaloneConfiguration("localhost", 6379);
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
 		return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 
-	
+	@Bean
+	RedisTemplate<String, Object> getRedisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+		template.setConnectionFactory(getJedisConnectionFactory());
+		return template;
+
+	}
+
 }
