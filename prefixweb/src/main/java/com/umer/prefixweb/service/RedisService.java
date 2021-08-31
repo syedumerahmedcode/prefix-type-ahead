07a991aa8485dc35228ff8f9da4f7c840c29ae39
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-31 13:24:29 +0200

    Feature: Extracted the Redis Cache logic into its own separate service.

--- prefixweb/src/main/java/com/umer/prefixweb/service/RedisService.java
+++ prefixweb/src/main/java/com/umer/prefixweb/service/RedisService.java
@@ -1,8 +1,24 @@
 package com.umer.prefixweb.service;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisService {
+	
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+
+	public void insertIntoCache(String prefix) {
+		redisTemplate.opsForZSet().add(prefix, prefix, 1);
+	}
+	
+	public Set<String> checkCachePrefix(String prefix) {
+		Set<String> results = redisTemplate.opsForZSet().reverseRange(prefix, 0, -1);
+		return results;
+	}
 
 }
--- prefixweb/src/main/java/com/umer/prefixweb/service/TrieService.java
+++ prefixweb/src/main/java/com/umer/prefixweb/service/TrieService.java
@@ -5,14 +5,13 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import data.PrefixTrie;
 
 /**
  * Communicate back and forth with the current running instance of the Trie
- * representng the current range.
+ * representing the current range.
  * 
  * @author umer
  *
@@ -26,15 +25,15 @@ public class TrieService {
 	@Autowired
 	private RedisService redisService;
 
-	@Autowired
-	private RedisTemplate<String, String> redisTemplate;
+//	@Autowired
+//	private RedisTemplate<String, String> redisTemplate;
 
 	public TrieService() {
 		this.prefixTrie = new PrefixTrie();
 	}
 
 	public List<String> getMatchingPhrases(String prefix) {
-		Set<String> results = checkCachePrefix(prefix);
+		Set<String> results = redisService.checkCachePrefix(prefix);
 		if (results.size() > 0) {
 			return new ArrayList<String>(results);
 		} else {
@@ -43,10 +42,10 @@ public class TrieService {
 
 	}
 
-	private Set<String> checkCachePrefix(String prefix) {
-		Set<String> results = redisTemplate.opsForZSet().reverseRange(prefix, 0, -1);
-		return results;
-	}
+//	private Set<String> checkCachePrefix(String prefix) {
+//		Set<String> results = redisTemplate.opsForZSet().reverseRange(prefix, 0, -1);
+//		return results;
+//	}
 
 	public void insertPrefix(String prefix) {
 		prefixTrie.insertPrefix(prefix);
@@ -54,12 +53,12 @@ public class TrieService {
 		 * Once a prefix is inserted into the Trie for the first time, w we want to
 		 * inject it into the cache as well.
 		 */
-		insertIntoCache(prefix);
+		redisService.insertIntoCache(prefix);
 
 	}
 
-	private void insertIntoCache(String prefix) {
-		redisTemplate.opsForZSet().add(prefix, prefix, 1);
-	}
+//	private void insertIntoCache(String prefix) {
+//		redisTemplate.opsForZSet().add(prefix, prefix, 1);
+//	}
 
 }
